a26317692d4fb626253ac77bd4d7db41c02bf6bb
1234 <209410306@gms.tku.edu.tw>
2021-09-30 19:57:03 +0800

    first commit

---
+++ src/App_06.js
@@ -0,0 +1,15 @@
+import Todo_06 from './components/Todo_06';
+import './App.css';
+
+function App_06() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo_06 text='Learn React' />
+      <Todo_06 text='Learn SQK' />
+      <Todo_06 text='Learn Nextjs' />
+    </div>
+  );
+}
+
+export default App_06;
